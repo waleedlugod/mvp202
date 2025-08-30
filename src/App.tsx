6ecff1a2353c9888587102444b67213dd0346055
Waleed Lugod <lugodwaleed@gmail.com>
2025-08-31 01:30:01 +0800

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
 function App() {
-  return <></>;
+  return (
+    <Canvas id="webgl">
+      <OrbitControls />
+      <ambientLight />
+      <directionalLight position={[1, 2, 3]} intensity={2} />
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial color={"orange"} />
+      </mesh>
+    </Canvas>
+  );
 }
 
 export default App;
