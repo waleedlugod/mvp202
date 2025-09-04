68f558e460810b18c9fa8f7d7801f7a3bb158f50
Waleed Lugod <lugodwaleed@gmail.com>
2025-09-04 22:45:04 +0800

    fix analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { Canvas } from "@react-three/fiber";
 import { Gltf, OrbitControls } from "@react-three/drei";
 import {
@@ -27,49 +28,52 @@ function App() {
   }
 
   return (
-    <Canvas id="webgl" camera={{ position: [0, 0, 4] }}>
-      <OrbitControls
-        enableZoom={false}
-        enablePan={false}
-        enableRotate={false}
-      />
+    <>
+      <Analytics />
+      <Canvas id="webgl" camera={{ position: [0, 0, 4] }}>
+        <OrbitControls
+          enableZoom={false}
+          enablePan={false}
+          enableRotate={false}
+        />
 
-      <ambientLight intensity={0.8} color={"#ffc0cb"} />
-      <directionalLight position={[0, 2, 0]} intensity={1} />
-      <pointLight ref={pl} position={[1.5, 1.5, 1.5]} intensity={5} />
-      {debug && pl.current ? <pointLightHelper args={[pl.current]} /> : <></>}
+        <ambientLight intensity={0.8} color={"#ffc0cb"} />
+        <directionalLight position={[0, 2, 0]} intensity={1} />
+        <pointLight ref={pl} position={[1.5, 1.5, 1.5]} intensity={5} />
+        {debug && pl.current ? <pointLightHelper args={[pl.current]} /> : <></>}
 
-      <Physics debug={debug}>
-        {/* roof */}
-        <RigidBody type="fixed">
-          <mesh position={[0, 1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-            <planeGeometry args={[10, 10]} />
-            <meshBasicMaterial />
-          </mesh>
-        </RigidBody>
+        <Physics debug={debug}>
+          {/* roof */}
+          <RigidBody type="fixed">
+            <mesh position={[0, 1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+              <planeGeometry args={[10, 10]} />
+              <meshBasicMaterial />
+            </mesh>
+          </RigidBody>
 
-        {/* heart */}
-        <RigidBody
-          ref={rb}
-          enabledTranslations={[false, true, false]}
-          enabledRotations={[false, true, false]}
-          friction={0.2}
-          frictionCombineRule={CoefficientCombineRule.Min}
-          colliders={false} // use custom collider since auto is not centered
-        >
-          <CuboidCollider args={[1, 1, 1]} />
-          <Gltf src={"/assets/scene.gltf"} scale={0.01} onClick={jump} />
-        </RigidBody>
+          {/* heart */}
+          <RigidBody
+            ref={rb}
+            enabledTranslations={[false, true, false]}
+            enabledRotations={[false, true, false]}
+            friction={0.2}
+            frictionCombineRule={CoefficientCombineRule.Min}
+            colliders={false} // use custom collider since auto is not centered
+          >
+            <CuboidCollider args={[1, 1, 1]} />
+            <Gltf src={"/assets/scene.gltf"} scale={0.01} onClick={jump} />
+          </RigidBody>
 
-        {/* floor */}
-        <RigidBody type="fixed">
-          <mesh position={[0, -2, 0]} rotation={[Math.PI / 2, 0, 0]}>
-            <planeGeometry args={[10, 10]} />
-            <meshBasicMaterial />
-          </mesh>
-        </RigidBody>
-      </Physics>
-    </Canvas>
+          {/* floor */}
+          <RigidBody type="fixed">
+            <mesh position={[0, -2, 0]} rotation={[Math.PI / 2, 0, 0]}>
+              <planeGeometry args={[10, 10]} />
+              <meshBasicMaterial />
+            </mesh>
+          </RigidBody>
+        </Physics>
+      </Canvas>
+    </>
   );
 }
 
